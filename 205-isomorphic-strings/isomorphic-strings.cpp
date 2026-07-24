cd8fffd1b9968cda9ede84d44196468924b5fa56
Avni Ruhela <avniruhela0@gmail.com>
2026-07-24 20:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (98.81%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int indexS[200] = {0};
+        int indexT[200] = {0};
+        int len = s.length();
+        
+        if (len != t.length()) {
+            return false;
+        }
+        
+        for (int i = 0; i < len; i++) {
+            if (indexS[s[i]] != indexT[t[i]]) {
+                return false;
+            }
+            indexS[s[i]] = i + 1;
+            indexT[t[i]] = i + 1;
+        }
+        
+        return true;
+    }
+};
