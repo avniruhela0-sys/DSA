374a5e135be2fd9fcbac9d1d12063226ccba446e
Avni Ruhela <avniruhela0@gmail.com>
2026-08-15 20:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (23.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,25 +3,22 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-
-        vector<int> rowarray(m,1);
-        vector<int> colarray(n,1);
-        
+        vector<int> rowmat(m,1);
+        vector<int> colmat(n,1);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    rowarray[i]=0;
-                    colarray[j]=0;
+                    rowmat[i]=0;
+                    colmat[j]=0;
                 }
             }
         }
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if( rowarray[i]== 0 || colarray[j]==0){
+                if(rowmat[i]==0 || colmat[j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
     }
-}; 
+};
